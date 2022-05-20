8fa2dd59c8ed2b6231466e29c4e68c360e875b7f
Jonathan Tri <jonathantric@gmail.com>
2022-05-20 08:34:49 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jonathan Tri Christianto on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
